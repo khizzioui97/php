615a1de30dbc917b7e8576dbce958d40c579133e
el idrissi salmane <khiziouii@gmail.com>
2021-01-27 04:44:51 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+include('ninjas.php');
+require('ninjas.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Document</title>
+  </head>
+    <body>
+      <h2>
+       
+  
+      </h2>
+   </body>
+</html>
